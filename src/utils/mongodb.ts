68c2ae995c05f097ae4e361780b9972783e9c1fe
ArvindAgarwal1310 <arvindagarwal839@gmail.com>
2025-06-02 10:12:34 +0530

    WIP: Save local changes before pulling

---
+++ src/utils/mongodb.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = process.env.DATABASE_NAME;
+
+if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+let cachedClient: MongoClient | null = null;
+
+export async function connectToDatabase() {
+    if (cachedClient) {
+        return {
+            client: cachedClient,
+            db: cachedClient.db(DB_NAME),
+        };
+    }
+
+    const client = await MongoClient.connect(MONGODB_URI || '');
+    cachedClient = client;
+
+    return {
+        client,
+        db: client.db(DB_NAME),
+    };
+}
