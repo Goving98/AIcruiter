8b1828eb2817467f92ca92c9c06e2eb70b33fddf
Govindaaa <121843498+Goving98@users.noreply.github.com>
2025-06-05 01:31:52 +0530

    Updated auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,4 @@
-export const mockCredentials = {
-    student: {
-      email: "student@test.com",
-      password: "test123"
-    }
-  };
+
   
   export const validateStudent = (email: string, password: string) => {
     return email === mockCredentials.student.email && 
